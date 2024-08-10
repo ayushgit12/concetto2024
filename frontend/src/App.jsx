8353e7bd699f7bf484846b404d181fc7ae48afdf
Devraj <devrajbando@gmail.com>
2024-08-10 17:57:10 +0530

    merged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,25 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import Navbar from './components/Navbar'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-=======
+
 import { Routes, Route } from 'react-router-dom'
 import './index.css'
 import Dashboard from './managers/Dashboard.jsx'
+import Navbar from './components/Navbar'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
->>>>>>> f56ff1349a509e347bec0bdfce73761422a4ed9c
+
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-    <BrowserRouter>
+
+<BrowserRouter>
       <Navbar/>
-      </BrowserRouter>
-=======
       <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      </BrowserRouter>
       
->>>>>>> f56ff1349a509e347bec0bdfce73761422a4ed9c
     </>
   )
 }
