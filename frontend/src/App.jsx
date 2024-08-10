c88d6e4fa6caca5ccad1eb88511c16e5770b64e0
Vaibhaviitian <aryanvaibhav74@gmail.com>
2024-08-11 00:05:15 +0530

    adding something

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import Dashboard from "./managers/Dashboard.jsx";
@@ -7,40 +5,24 @@ import Register from "./components/User/Register.jsx";
 import Login from "./components/User/Login.jsx";
 import OTP from "./components/User/OTP.jsx";
 import OTPVerifed from "./components/User/OTPVerifed.jsx";
-=======
-import { useState } from 'react'
-
-import { Routes, Route } from 'react-router-dom'
-import './index.css'
-import Dashboard from './managers/Dashboard.jsx'
-import Navbar from './components/Navbar'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import Leaderboard from './managers/Leaderboard.jsx'
-
-
->>>>>>> e4da23c6746749745df9b36c7674507689781833
+import "./index.css";
+import Navbar from "./components/Navbar";
+import "./index.css";
+import Leaderboard from "./managers/Leaderboard.jsx";
 
 function App() {
   return (
     <>
-
-      <Navbar/>
+      <Navbar />
       <Routes>
-<<<<<<< HEAD
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/otp-verification" element={<OTP />} />
         <Route path="/otp-verified" element={<OTPVerifed />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
-=======
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      </Routes>
-    
-      
->>>>>>> e4da23c6746749745df9b36c7674507689781833
     </>
   );
 }
