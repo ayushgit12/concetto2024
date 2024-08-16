29f5a58afbca3e89998379569e67314ffdb467ea
ayushgit12 <ayush2005raj@gmail.com>
2024-08-16 22:20:40 +0530

    made landing page image not draggable

--- frontend/src/landing page/hero.jsx
+++ frontend/src/landing page/hero.jsx
@@ -5,11 +5,11 @@ import img from '/Concetto24.png';
 
 const Hero = () => {
   return (
-    <div className=' ' style={{height:`${window.innerHeight - 80}px`}}>
+    <div className='select-none' style={{height:`${window.innerHeight - 80}px`}}>
     <img
       src={img}
       alt="Header Image"
-      className="w-full h-full "
+      className="w-full h-full pointer-events-none"
       style={{backgroundSize:"100% 100%"}}
     />
   </div>
